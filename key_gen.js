b43fe3a84984cdb6796892461bf6d0292d80cca4
Ahaan Kanaujia <86679172+AhaanKanaujia@users.noreply.github.com>
2022-01-26 22:23:36 -0600

    Update key_gen.js

--- key_gen.js
+++ key_gen.js
@@ -5,6 +5,6 @@ const key = ec.genKeyPair();
 const public_key = key.getPublic('hex');
 const private_key = key.getPrivate('hex');
 
-console.log("Public Key: " + public_key)
+console.log("Public Key: " + public_key);
 console.log();
-console.log("Private Key: " + private_key)
+console.log("Private Key: " + private_key);
